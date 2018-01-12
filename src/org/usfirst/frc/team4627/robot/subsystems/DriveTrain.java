1f05bebc81d044f4fab67153a5966a0da6b47566
MarkiCosta <firesniper01@gmail.com>
2018-01-12 13:20:40 -0700

    Resolved errors

--- src/org/usfirst/frc/team4627/robot/subsystems/DriveTrain.java
+++ src/org/usfirst/frc/team4627/robot/subsystems/DriveTrain.java
@@ -2,17 +2,10 @@ package org.usfirst.frc.team4627.robot.subsystems;
 
 import org.usfirst.frc.team4627.robot.RobotMap;
 import org.usfirst.frc.team4627.robot.commands.TankDrive;
-
-<<<<<<< HEAD
-import edu.wpi.first.wpilibj.TalonSRX;
-=======
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.SPI;
-
->>>>>>> Autonomous
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -25,15 +18,12 @@ public class DriveTrain extends Subsystem {
 	TalonSRX leftMotor2 = new TalonSRX(RobotMap.LEFT_MOTOR_2);
 	TalonSRX rightMotor1 = new TalonSRX(RobotMap.RIGHT_MOTOR_1);
 	TalonSRX rightMotor2 = new TalonSRX(RobotMap.RIGHT_MOTOR_2);
-<<<<<<< HEAD
-=======
 	
 	//public static ADXRS450_Gyro gyro = new ADXRS450_Gyro(SPI.Port.kOnboardCS0);
 	
 	/*public double getGyroAngle() {
 		return gyro.getAngle();
 	}*/
->>>>>>> Autonomous
 	
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
