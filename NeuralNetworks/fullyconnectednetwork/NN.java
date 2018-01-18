2c687cb32afe07237ec6ec9886ec0df9222bc328
Lucas-Brown <33075651+Lucas-Brown@users.noreply.github.com>
2018-01-18 15:59:32 -0700

    the better one

---
+++ NeuralNetworks/fullyconnectednetwork/NN.java
@@ -0,0 +1,49 @@
+package fullyconnectednetwork;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import TrainSet.TrainSet;
+
+public class NN {
+
+    public Network net;
+    public TrainSet set;
+
+    public NN(int... NETWORK_LAYER_SIZES) {
+        this.net = new Network(NETWORK_LAYER_SIZES);
+        this.set = new TrainSet(this.net.INPUT_SIZE, this.net.OUTPUT_SIZE);
+    }
+
+    public NN(String netPath, String trainPath) {
+        try {
+            this.net = Network.loadNetwork(netPath);
+            this.set = new TrainSet(trainPath);
+        } catch (Exception ex) {
+            Logger.getLogger(NN.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void train(int loops){
+        net.train(this.set, loops, set.size());
+    }
+    
+    public void trainAndSave(int loops, int saveInterval, String file){
+        net.train(set, loops, set.size(), saveInterval, file);
+    }
+    
+    public void addData(double[] input, double[] expected){
+        this.set.addData(input, expected);
+    }
+    
+    public void saveSet(String path){
+        this.set.saveTrainSet(path);
+    }
+    
+    public void saveNet(String path) throws Exception{
+        this.net.saveNetwork(path);
+    }
+    
+    public double[] calculate(double[] input){
+        return this.net.calculate(input);
+    }
+}
