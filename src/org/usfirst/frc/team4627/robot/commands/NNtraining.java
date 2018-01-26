edc62ab87d67576b109130134d8fbb1da40c54fd
Lucas-Brown <33075651+Lucas-Brown@users.noreply.github.com>
2018-01-25 21:04:58 -0700

    NN turn data
    TurnAndAddData class now collects data on turning and saves it into the roborio.

--- src/org/usfirst/frc/team4627/robot/commands/NNtraining.java
+++ src/org/usfirst/frc/team4627/robot/commands/NNtraining.java
@@ -24,7 +24,7 @@ public class NNtraining extends CommandGroup {
         // e.g. if Command1 requires chassis, and Command2 requires arm,
         // a CommandGroup containing them would require both the chassis and the
         // arm.
-    	addSequential(new TurnAndAddData(1, true));
+    	addSequential(new TurnAndAddData());
     	
     }
 }
