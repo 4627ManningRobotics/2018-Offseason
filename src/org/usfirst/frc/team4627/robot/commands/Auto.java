8003d33825cb72d0b4a7f1023436a3968ba42d77
MarkiCosta <firesniper01@gmail.com>
2018-02-18 11:13:18 -0700

    Do not revert to this commit

--- src/org/usfirst/frc/team4627/robot/commands/Auto.java
+++ src/org/usfirst/frc/team4627/robot/commands/Auto.java
@@ -9,18 +9,24 @@ public class Auto extends CommandGroup {
 
     public Auto() {
 <<<<<<< HEAD
+<<<<<<< HEAD
     	addSequential(new DriveForward(0.2, 0.2, 1));
     	addSequential(new TurnToAngle(45, RobotMap.TURN_SPEED, 2));
     	addSequential(new DriveForward(0.4, 0.4, 1));
     	addSequential(new TurnToAngle(-47, 0.2, 5));
     	addSequential(new DriveForward(0.3, 0.3, 1.47));
 =======
+=======
+>>>>>>> parent of 8bd672d... Works better now
     	//addSequential(new DriveForward(0.2, 0.2, 1));
     	addSequential(new TurnToAngle(45, 0.3, 2));
     	//addSequential(new DriveForward(0.4, 0.4, 1));
     	//addSequential(new TurnToAngle(-45, 0.35, 2));
     	//addSequential(new DriveForward(0.17, 0.17, 1));
+<<<<<<< HEAD
 >>>>>>> 24cc9becd8d2d692c7be0e0f35654cf4ba31d064
+=======
+>>>>>>> parent of 8bd672d... Works better now
         // Add Commands here:
         // e.g. addSequential(new Command1());
         //      addSequential(new Command2());
