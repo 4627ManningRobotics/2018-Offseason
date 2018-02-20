c2162932e7e03404a4ce69473a231609f515807a
Lucas-Brown <33075651+Lucas-Brown@users.noreply.github.com>
2018-02-20 10:26:25 -0700

    yes

--- src/org/usfirst/frc/team4627/robot/subsystems/LeftArm.java
+++ src/org/usfirst/frc/team4627/robot/subsystems/LeftArm.java
@@ -39,7 +39,7 @@ public class LeftArm extends PIDSubsystem {
 	@Override
 	protected void usePIDOutput(double output) {
 		// TODO Auto-generated method stub
-		if((this.calculatePosition() < 10 && output < 0) || (this.calculatePosition() > 150 && output > 0)) {
+		if((this.calculatePosition() < 15 && output < 0) || (this.calculatePosition() > 145 && output > 0)) {
 			this.liftingMotor.set(this.liftingMotor.getControlMode(), 0);
 		}else{
 			this.liftingMotor.set(liftingMotor.getControlMode(), output);
