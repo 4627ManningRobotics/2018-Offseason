852bded9b8879ef74195b3348c2ad45585b24899
Lucas-Brown <33075651+Lucas-Brown@users.noreply.github.com>
2018-02-20 20:33:26 -0700

    yes

--- src/org/usfirst/frc/team4627/robot/commands/OperatorControls.java
+++ src/org/usfirst/frc/team4627/robot/commands/OperatorControls.java
@@ -23,25 +23,28 @@ public class OperatorControls extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if(Robot.oi.getOperatorBumper(RobotMap.RIGHT_BUMPER)) {
+    	if(Robot.oi.getOperatorBumper(RobotMap.RIGHT_BUMPER)) { // controls for in-take, full speed out
     		Robot.clamp.setLeftMotor(0.5);
     		Robot.clamp.setRightMotor(0.5);
-    	} else if(Robot.oi.getOperatorBumper(RobotMap.LEFT_BUMPER)) {
+    	} else if(Robot.oi.getOperatorBumper(RobotMap.LEFT_BUMPER)) { // full out
     		Robot.clamp.setLeftMotor(-0.5);
     		Robot.clamp.setRightMotor(-0.5);
-    	} else {
+    	} else { // trigger based
     		double triggerVal = Robot.oi.getOperatorRawAxis(RobotMap.RIGHT_TRIGGER) - Robot.oi.getOperatorRawAxis(RobotMap.LEFT_TRIGGER);
     		double stick = Robot.oi.getOperatorRawAxis(RobotMap.LEFT_STICK_X) * RobotMap.TURNING_RATE;
         	Robot.clamp.setLeftMotor(triggerVal + stick);
         	Robot.clamp.setRightMotor(triggerVal - stick);
     	}
     	
-    	if(DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'L') {
+    	if(DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'L') { // control of the arm using the left stick
     		System.out.println(Robot.leftArm.calculatePosition());
-    		Robot.leftArm.liftingMotor.set(Robot.leftArm.liftingMotor.getControlMode(), Robot.oi.getOperatorRawAxis(RobotMap.RIGHT_TRIGGER) - Robot.oi.getOperatorRawAxis(RobotMap.LEFT_TRIGGER));
+    		Robot.leftArm.liftingMotor.set(Robot.leftArm.liftingMotor.getControlMode(), Robot.oi.getDriverRawAxis(RobotMap.LEFT_STICK_Y));
 		}else if(DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'R') {
     		System.out.println(Robot.rightArm.calculatePosition());
-    		Robot.rightArm.liftingMotor.set(Robot.rightArm.liftingMotor.getControlMode(), Robot.oi.getOperatorRawAxis(RobotMap.RIGHT_TRIGGER) - Robot.oi.getOperatorRawAxis(RobotMap.LEFT_TRIGGER));
+    		Robot.rightArm.liftingMotor.set(Robot.rightArm.liftingMotor.getControlMode(), Robot.oi.getOperatorRawAxis(RobotMap.LEFT_STICK_Y));
+		}else if(DriverStation.getInstance().getGameSpecificMessage().charAt(0) == 'B') {
+    		Robot.rightArm.liftingMotor.set(Robot.rightArm.liftingMotor.getControlMode(), Robot.oi.getOperatorRawAxis(RobotMap.LEFT_STICK_Y));
+    		Robot.leftArm.liftingMotor.set(Robot.leftArm.liftingMotor.getControlMode(), Robot.oi.getOperatorRawAxis(RobotMap.LEFT_STICK_Y));
 		}
     }
 
