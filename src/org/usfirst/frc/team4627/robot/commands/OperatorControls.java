9792d88b41a2d49bb1e63667b278c2210753df90
MarkiCosta <firesniper01@gmail.com>
2018-02-27 17:13:11 -0700

    no changes

---
+++ src/org/usfirst/frc/team4627/robot/commands/OperatorControls.java
@@ -0,0 +1,63 @@
+package org.usfirst.frc.team4627.robot.commands;
+
+import org.usfirst.frc.team4627.robot.Robot;
+import org.usfirst.frc.team4627.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class OperatorControls extends Command {
+
+    public OperatorControls() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.arm);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	//clamp controls
+    	if(Robot.oi.getOperatorButton(RobotMap.RIGHT_BUMPER)) {
+    		Robot.arm.clamp.setLeftMotor(RobotMap.CLAMP_MAX_SPEED);
+    		Robot.arm.clamp.setRightMotor(RobotMap.CLAMP_MAX_SPEED);
+    	} else if(Robot.oi.getOperatorButton(RobotMap.LEFT_BUMPER)) {
+    		Robot.arm.clamp.setLeftMotor(-RobotMap.CLAMP_MAX_SPEED);
+    		Robot.arm.clamp.setRightMotor(-RobotMap.CLAMP_MAX_SPEED);
+    	} else {
+    		double triggerVal = Robot.oi.getOperatorRawAxis(RobotMap.RIGHT_TRIGGER) - Robot.oi.getOperatorRawAxis(RobotMap.LEFT_TRIGGER);
+    		double stick = Robot.oi.getOperatorRawAxis(RobotMap.LEFT_STICK_X) * RobotMap.TURNING_RATE;
+        	Robot.arm.clamp.setLeftMotor((triggerVal + stick) * RobotMap.CLAMP_MAX_SPEED);
+        	Robot.arm.clamp.setRightMotor((triggerVal - stick) * RobotMap.CLAMP_MAX_SPEED);
+    	}
+    	if(Robot.oi.getOperatorAButton()) {
+    		//Robot.arm.setSetpoint(70);
+    		//Robot.arm.enable();
+    	}
+    	//Robot.arm.rightLiftingMotor.set(Robot.arm.leftLiftingMotor.getControlMode(), Robot.oi.getOperatorRawAxis(RobotMap.RIGHT_TRIGGER) - Robot.oi.getOperatorRawAxis(RobotMap.LEFT_TRIGGER));
+    	//Robot.arm.leftLiftingMotor.set(Robot.arm.rightLiftingMotor.getControlMode(), Robot.oi.getOperatorRawAxis(RobotMap.RIGHT_TRIGGER) - Robot.oi.getOperatorRawAxis(RobotMap.LEFT_TRIGGER));
+    	
+    	
+    	//wrist controls
+    	//Robot.arm.setWrist(Robot.oi.getOperatorRawAxis(RobotMap.RIGHT_STICK_Y));
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
