8116a693b10c24cc01b92965eb6779a0508efa5e
MarkiCosta <firesniper01@gmail.com>
2018-03-01 16:58:17 -0700

    sadfgh

--- src/org/usfirst/frc/team4627/robot/subsystems/Wrist.java
+++ src/org/usfirst/frc/team4627/robot/subsystems/Wrist.java
@@ -2,65 +2,43 @@ package org.usfirst.frc.team4627.robot.subsystems;
 
 import org.usfirst.frc.team4627.robot.RobotMap;
 
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
  *
  */
+public class Wrist extends Subsystem {
 
-public class Wrist extends PIDSubsystem {
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
 
-	private final TalonSRX wrist = new TalonSRX(RobotMap.WRIST_MOTOR); //ppr = 1024
+	public TalonSRX wrist = new TalonSRX(RobotMap.WRIST_MOTOR);
+	private double p = 0; // TODO
+	public boolean isInTolerance = false;
+	public double targetCurrent;
 	
-    public Wrist(double P, double I, double D) {
-        super("Wrist", P, I, D);
-        //this.wrist.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 0);
-		super.getPIDController().setAbsoluteTolerance(0.05);
-		super.getPIDController().setContinuous(false); // does not wrap
-		super.getPIDController().setOutputRange(-1, 1); // CBW- can use this to set min max motor speed?
-		super.getPIDController().setInputRange(-180, 180);
-		
-        int absolutePosition = this.wrist.getSensorCollection().getPulseWidthPosition();
-		/* mask out overflows, keep bottom 12 bits */
-		absolutePosition &= 0xFFF; // mask/bitwise operator, filters out unnecessary data
-		
-		//if (Constants.kSensorPhase)
-			//absolutePosition *= -1;
-			//absolutePosition *= -1;
-		/* set the quadrature (relative) sensor to match absolute */
-		this.wrist.setSelectedSensorPosition(-absolutePosition, 0, 10); //absolute position is the start position of the potentiometer
-		//																 2nd parameter is index (don't change it)
-		//																 3rd parameter is time in milliseconds for the calculation to be completed
-		// Use these to get going:
-        // setSetpoint() -  Sets where the PID controller should move the system
-        //                  to
-        // enable() - Enables the PID controller.
+	public Wrist() {
+		this.wrist.setSelectedSensorPosition(-this.wrist.getSensorCollection().getPulseWidthPosition(), 0, 10);
+	}
+	
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new WristPID());
     }
     
-    public double wristAmperage() {
+    public double getCurrent() {
     	return this.wrist.getOutputCurrent();
     }
-
-    public void initDefaultCommand() {
-    	// not needed
+    
+    public double getAngle() {
+    	return -this.wrist.getSelectedSensorPosition(0)/10d;
     }
     
-    @Override
-    protected double returnPIDInput() {
-        return 0;//this.calculateAngle();
+    public double calculateError() {
+    	return Math.abs((this.wrist.getOutputCurrent() - this.targetCurrent)/this.targetCurrent);
     }
-    @Override
-    protected void usePIDOutput(double output) {
-    	this.wrist.set(this.wrist.getControlMode(), output);
-    }
-    
-    public void setWrist(double speed) {
-    	this.wrist.set(this.wrist.getControlMode(), speed);
-    }
-    /*public double calculateAngle() {
-    	return -this.wrist.getSelectedSensorPosition(0) / 10d;
-    }*/
 }
+
