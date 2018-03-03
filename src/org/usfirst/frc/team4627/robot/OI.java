2930054602b6d4514836ea2cb06c6f8b4f4cd289
MarkiCosta <firesniper01@gmail.com>
2018-03-03 09:40:07 -0700

    Assigned CommandGroups to buttons

--- src/org/usfirst/frc/team4627/robot/OI.java
+++ src/org/usfirst/frc/team4627/robot/OI.java
@@ -1,8 +1,12 @@
 package org.usfirst.frc.team4627.robot;
 
-import edu.wpi.first.wpilibj.XboxController;
 
-//import org.usfirst.frc.team4627.robot.commands.*;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+
+import org.usfirst.frc.team4627.robot.commands.*;
 
 
 /**
@@ -11,14 +15,14 @@ import edu.wpi.first.wpilibj.XboxController;
  */
 public class OI {
 
-	//// CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a
-	//// joystick.
-	// You create one by telling it which joystick it's on and which button
-	// number it is.
+	
 	XboxController driverController = new XboxController(RobotMap.DRIVER_CONTROLLER);
 	XboxController operatorController = new XboxController(RobotMap.OPERATOR_CONTROLLER);
 	
+	Button oButtonA = new JoystickButton(operatorController, RobotMap.BUTTON_A);
+	Button oButtonB = new JoystickButton(operatorController, RobotMap.BUTTON_B);
+	Button oButtonY = new JoystickButton(operatorController, RobotMap.BUTTON_Y);
+
 	public boolean getOperatorButton(int axis) {
 		return this.operatorController.getRawButtonPressed(axis);
 	}
@@ -35,25 +39,13 @@ public class OI {
 		return this.driverController.getRawAxis(axis);
 	}
 	
-	// There are a few additional built in buttons you can use. Additionally,
-	// by subclassing Button you can create custom triggers and bind those to
-	// commands the same as any other Button.
 
-	//// TRIGGERING COMMANDS WITH BUTTONS
-	// Once you have a button, it's trivial to bind it to a button in one of
-	// three ways:
-
-	// Start the command when the button is pressed and let it run the command
-	// until it is finished as determined by it's isFinished method.
 	public OI () {
-	
+		oButtonA.whenPressed(new TeleOpGoToGround());
+		oButtonB.whenPressed(new TeleOpGoToSwitch());
+		oButtonY.whenPressed(new TeleOpGoToScale());
+
 	}
 
-	// Run the command while the button is being held down and interrupt it once
-	// the button is released.
-	// button.whileHeld(new ExampleCommand());
 
-	// Start the command when the button is released and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenReleased(new ExampleCommand());
 }
