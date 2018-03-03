816f77a7cf38b11945538d52ecd31903ad339ccd
MarkiCosta <firesniper01@gmail.com>
2018-03-03 09:26:08 -0700

    Added Control Commands

---
+++ src/org/usfirst/frc/team4627/robot/commands/SetArm.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team4627.robot.commands;
+
+import org.usfirst.frc.team4627.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+//Sets the arm setpoint and ends immediately
+
+public class SetArm extends Command {
+	double m_setpoint;
+
+    public SetArm(double setpoint) {
+    	m_setpoint = setpoint;
+        requires(Robot.rightArm);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	Robot.rightArm.setSetpoint(m_setpoint);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
