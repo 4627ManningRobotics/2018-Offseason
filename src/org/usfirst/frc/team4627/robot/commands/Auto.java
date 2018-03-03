cd4a2fe2e40b68afa822b0d19a478ea576d75580
MarkiCosta <firesniper01@gmail.com>
2018-03-03 12:22:31 -0700

    Adapted Auto to new arm control

--- src/org/usfirst/frc/team4627/robot/commands/Auto.java
+++ src/org/usfirst/frc/team4627/robot/commands/Auto.java
@@ -10,8 +10,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class Auto extends CommandGroup {
 
     public Auto() {
-    	addSequential(new SetArm(RobotMap.WRIST_SWITCH));
-    	addSequential(new SetWrist(RobotMap.WRIST_SWITCH)); // to run along side the sequential
+    	addSequential(new GoToSwitch()); // to run along side the sequential
     	addSequential(new DriveForward(0.2, 0.2, 1));
     	addSequential(new TurnToAngle(true, 45, RobotMap.TURN_SPEED, 2));
     	addSequential(new DriveForward(0.4, 0.4, 1));
