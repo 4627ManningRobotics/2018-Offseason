d6f762d0a77d9278fe3cf629117133304aca2bbe
Lucas-Brown <33075651+Lucas-Brown@users.noreply.github.com>
2018-03-05 17:15:09 -0700

    detailed commit

--- src/org/usfirst/frc/team4627/robot/commands/SetArm.java
+++ src/org/usfirst/frc/team4627/robot/commands/SetArm.java
@@ -27,31 +27,43 @@ public class SetArm extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	if(Robot.wrist.onTarget() && !this.wDead1) {
+    	if(this.isWristStowed() && !this.wDead1) {
     		this.wDead1 = true;
     	}
-    	if(this.isInDeadzone()) {
+    	if(this.isInDeadzone() && !this.isWristStowed()) {
     		Robot.rightArm.setSetpointRelative(0); // if in dead zone stop moving
     	}else if(this.wDead1){
     		Robot.rightArm.setSetpoint(this.m_setpoint);
+    	}else if(!this.isInDeadzone() && !this.wDead1){
+    		this.wDead1 = true;
     	}
     }
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return true;
+        return Robot.rightArm.onTarget() && Robot.leftArm.onTarget();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.rightArm.disable();
+    	Robot.leftArm.disable();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    	this.end();
     }
     
     private boolean isInDeadzone() {
     	return Robot.rightArm.getPosition() > RobotMap.ARMS_DEADZONE_MIN && Robot.rightArm.getPosition() < RobotMap.ARMS_DEADZONE_MAX;
+    }
+    
+    private boolean isWristStowed() {
+    	return (RobotMap.WRIST_DOWN_STOW <= Robot.wrist.getPosition() + RobotMap.WRIST_TOLERANCE_LEVEL &&
+    			RobotMap.WRIST_DOWN_STOW >= Robot.wrist.getPosition() - RobotMap.WRIST_TOLERANCE_LEVEL) || 
+    		   (RobotMap.WRIST_UP_STOW <= Robot.wrist.getPosition() + RobotMap.WRIST_TOLERANCE_LEVEL && 
+    			RobotMap.WRIST_UP_STOW >= Robot.wrist.getPosition() - RobotMap.WRIST_TOLERANCE_LEVEL);
     }
 }
