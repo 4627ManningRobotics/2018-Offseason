85ab941b849315562baf0f2b7bccc2cf5c0768a4
JoeTodes <joe.eng.wilson@gmail.com>
2018-03-26 14:09:03 -0600

    going back...

--- src/org/usfirst/frc/team4627/robot/commands/Auto.java
+++ src/org/usfirst/frc/team4627/robot/commands/Auto.java
@@ -9,16 +9,16 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class Auto extends CommandGroup {
 
-    public Auto() {
+    public Auto() { // currently time based
     	addParallel(new GoToSwitch()); // move the arm/wrist as the robot moves
     	
-    	addSequential(new  DriveForward(0.5, 0.3));
-    	//addSequential(new  TurnToAngleFMS(45,  0.2,  0.5,  3));
-    	addSequential(new TurnToAngleTimeFMS(0.35, 0.75)); // turn
-    	addSequential(new DriveForwardFMS(0.75, 0.75, 0.2));
-    	//addSequential(new  TurnToAngleFMS(-47,  0.2,  0.5,  3));
-    	addSequential(new TurnToAngleTimeFMS(0.4, -0.75)); // turn
-    	addSequential(new  DriveForwardFMS(0.75, .3, .1));
+    	addSequential(new DriveForward(0.4, 0.3));
+    	//addSequential(new TurnToAngleFMS(45,  0.2,  0.5,  3));
+    	addSequential(new TurnToAngleTimeFMS(0.4, 0.5)); // turn
+    	addSequential(new DriveForwardFMS(0.5, 0.9, 0.2));
+    	//addSequential(new TurnToAngleFMS(-47,  0.2,  0.5,  3));
+    	addSequential(new TurnToAngleTimeFMS(0.4, -0.5)); // turn
+    	addSequential(new DriveForwardFMS(0.7, .2, .1));
     	/*
     	addSequential(new DriveForward(0.25, 0.25, 4)); //1s
     	addSequential(new TurnToAngleFMS(43, RobotMap.TURN_SPEED, 2));
@@ -30,7 +30,12 @@ public class Auto extends CommandGroup {
     	*/
     	addSequential(new Wait(0.25)); // wait for the robot to stop
     	addSequential(new ReleaseBox());    
-    	addSequential(new TurnToAngleFMS(-10, RobotMap.TURN_SPEED, 2));
+    	//addSequential(new TurnToAngleFMS(-10, RobotMap.TURN_SPEED, 2));
+    	
+    	addSequential(new DriveForward(-0.5, 0.5));
+    	addSequential(new TurnToAngleTimeFMS(0.4, 0.75)); // turn
+    	addSequential(new DriveForwardFMS(-0.75, 0.5, 0.2));
+    	addSequential(new TurnToAngleTimeFMS(0.4, -0.75)); // turn
     	
     }
 }
